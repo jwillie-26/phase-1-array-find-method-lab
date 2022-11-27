3910191adae2afb33c3cd168310ba7a819fbe119
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-11-27 13:29:22 +0300

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,18 @@
+//Find method
+
+const testVar = {}
+
+//Solution code
+
+function testFunc() {
+    return "hi"
+  }
+  
+  function superbowlWin(objArr) {
+    const winYear = objArr.find( e => e.result === "W")
+    if(winYear) {
+      return winYear.year
+    } else {
+      return undefined
+    }
+  }
